8005f51cb8c438a5d47a84c332344f4fe785bb5b
Dhani Al G <84299714+DhaniAL-G@users.noreply.github.com>
2025-08-25 18:25:03 +0700

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
     
-    // Format input kartu kredit
+    // Format input credit card
     const cardNumber = document.getElementById('cardNumber');
     if (cardNumber) {
         cardNumber.addEventListener('input', function(e) {
